24b7ba5302d10920f18b4e75f413682f89be3841
Josh Kinney <joshkinney@kinneykare.com>
2021-03-24 18:23:05 -0500

    Initial Commit

---
+++ SwiftUIForms/SwiftUIFormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormsApp.swift
+//  SwiftUIForms
+//
+//  Created by Kinney Kare on 3/24/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
