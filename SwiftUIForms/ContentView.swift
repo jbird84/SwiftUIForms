91e7870a7afd795bb262cca1429c2cfd86086cd1
Josh Kinney <joshkinney@kinneykare.com>
2021-03-24 19:55:51 -0500

    Youtube Lesson pushed out

--- SwiftUIForms/ContentView.swift
+++ SwiftUIForms/ContentView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var theUser = ""
+    @State var airplaneModeIsOn = true
+    @State var pushNotificationsAreOn = true
+    @State private var previewIndex = 0
+    var previewOptions = ["Always", "When Unlocked", "Never"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("PROFILE")) {
+                    TextField("User", text: $theUser)
+                    HStack {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.orange)
+                                .cornerRadius(10)
+                            
+                            Image(systemName: "airplane")
+                                .foregroundColor(.white)
+                        }
+                        Toggle("Airplane Mode", isOn: $airplaneModeIsOn)
+                    }
+                }
+                Section(header: Text("NOTIFICATIONS")) {
+                    Toggle("Push Notifications", isOn: $pushNotificationsAreOn)
+                    Picker(selection: $previewIndex, label: Text("Show Previews")) {
+                        ForEach(0 ..< previewOptions.count) {
+                            Text(previewOptions[$0])
+                        }
+                    }
+                }
+                Section(header: Text("ABOUT")) {
+                    HStack {
+                        Text("Version")
+                        Spacer()
+                        Text("2.0.0")
+                    }
+                }
+                Section {
+                    
+                    HStack {
+                        ZStack {
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.gray)
+                                .cornerRadius(10)
+                            
+                            Image(systemName: "gear")
+                                .foregroundColor(.white)
+                        }
+                            
+                        
+                        Button(action: {
+                            //Code goes here
+                            print("All Settings Have Been Restored To Factory Settings!")
+                        }, label: {
+                            Text("Erase all Content and Settings")
+                    })
+                    }
+                }
+            }
+            .navigationBarTitle("Settings")
+        }
     }
 }
 
